6a72e575e1ef88686765724348d0512deff23c9c
rahulpakhare <rahulvpakhare@gmail.com>
2021-06-09 20:36:12 +0530

    Add gson for parsing response json to model object

--- app/src/main/java/com/artivatic/assignment/repo/ManualParsingImpl.kt
+++ app/src/main/java/com/artivatic/assignment/repo/ManualParsingImpl.kt
@@ -3,6 +3,7 @@ package com.artivatic.assignment.repo
 import com.artivatic.assignment.api.DataApi
 import com.artivatic.assignment.model.Data
 import com.artivatic.assignment.model.RowsItem
+import com.google.gson.Gson
 import org.json.JSONObject
 import java.io.BufferedInputStream
 import java.net.URL
@@ -27,16 +28,6 @@ class ManualParsingImpl : DataApi {
 
         val responseJson = stringBuffer.toString()
 
-        // json parsing
-        val jsonObjectData = JSONObject(responseJson)
-        val jsonArray = jsonObjectData.getJSONArray("rows")
-        val jsonObjectRow = jsonArray.getJSONObject(0)
-
-        val dataTitle = jsonObjectData.getString("title")
-        val rowTitle = jsonObjectRow.getString("title")
-        val description = jsonObjectRow.getString("description")
-        val image = jsonObjectRow.getString("imageHref")
-
-        return Data(dataTitle, listOf(RowsItem(rowTitle, description, image)))
+        return Gson().fromJson(responseJson, Data::class.java)
     }
 }
